4089a7c4d0411cd288f20dafb6a749907a8fe757
samglanfield <100288510+samglanfield@users.noreply.github.com>
2022-11-15 06:34:50 +0000

    Update Uniform Rejection Sampler - FIFA.R

--- Uniform Rejection Sampler - FIFA.R
+++ Uniform Rejection Sampler - FIFA.R
@@ -3,14 +3,15 @@ set.seed(1926002)
 initialgroups = array(rep(NA,16),dim=c(1,3,16)) #Gives us all groups but are empty
 total=0
 draws=list()
+totalvalid=0
 
-probofvalid=c()
+probofvalid=0;validity=0
 
-countUniform1=0;countUniform2=0;countUniform3=0;countUniform4=0;countUniform5=0;countUniform6=0;countUniform7=0;countUniform8=0;countUniform9=0;countUniform10=0;countUniform11=0;countUniform12=0;countUniform13=0;countUniform14=0;countUniform15=0;countUniform16=0;countUniform17=0;countUniform18=0
-countUniform19=0;countUniform20=0;countUniform21=0;countUniform22=0;countUniform23=0;countUniform24=0;countUniform25=0;countUniform26=0;countUniform27=0;countUniform28=0;countUniform29=0;countUniform30=0;countUniform31=0;countUniform32=0;countUniform33=0;countUniform34=0;countUniform35=0;countUniform36=0
+# countUniform1=0;countUniform2=0;countUniform3=0;countUniform4=0;countUniform5=0;countUniform6=0;countUniform7=0;countUniform8=0;countUniform9=0;countUniform10=0;countUniform11=0;countUniform12=0;countUniform13=0;countUniform14=0;countUniform15=0;countUniform16=0;countUniform17=0;countUniform18=0
+# countUniform19=0;countUniform20=0;countUniform21=0;countUniform22=0;countUniform23=0;countUniform24=0;countUniform25=0;countUniform26=0;countUniform27=0;countUniform28=0;countUniform29=0;countUniform30=0;countUniform31=0;countUniform32=0;countUniform33=0;countUniform34=0;countUniform35=0;countUniform36=0
 
 
-for(r in 1:10000000){
+while(length(draws)<1000){
   
   pot1 = c("CONMEBOL1","EU1","CONMEBOL2","EU2","EU3","EU4","EU5","EU6","EU7","EU8","EU9","EU10","CONCACAF1","CONMEBOL3","CONCACAF2","CONCACAF3")
   pot2 = c("EU11","CONMEBOL4","CAF1","EU12","AFC1","EU13","CAF2","CONMEBOL5","AFC2","EU14","EU15","EU16","AFC3","CONMEBOL6","CAF3","CONMEBOL7")
@@ -61,55 +62,56 @@ for(r in 1:10000000){
     #  } else{valid[k]=0}
   } 
   
-  validity[r] = prod(valid) 
-  if(validity[r]==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "CAF1" %in% initialgroups[1,,k]){countUniform1=countUniform1+1}}}
-  if(validity[r]==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "EU11" %in% initialgroups[1,,k]){countUniform2=countUniform2+1}}}
-  if(validity[r]==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "CONMEBOL4" %in% initialgroups[1,,k]){countUniform3=countUniform3+1}}}
-  if(validity[r]==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "AFC1" %in% initialgroups[1,,k]){countUniform4=countUniform4+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "CAF1" %in% initialgroups[1,,k]){countUniform5=countUniform5+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "AFC1" %in% initialgroups[1,,k]){countUniform6=countUniform6+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "EU11" %in% initialgroups[1,,k]){countUniform7=countUniform7+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "CONMEBOL4" %in% initialgroups[1,,k]){countUniform8=countUniform8+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "CAF1" %in% initialgroups[1,,k]){countUniform9=countUniform9+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "EU11" %in% initialgroups[1,,k]){countUniform10=countUniform10+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "AFC1" %in% initialgroups[1,,k]){countUniform11=countUniform11+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform12=countUniform12+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform13=countUniform13+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform14=countUniform14+1}}}
-  if(validity[r]==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform15=countUniform15+1}}}
-  if(validity[r]==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform16=countUniform16+1}}}
-  if(validity[r]==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform17=countUniform17+1}}}
-  if(validity[r]==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform18=countUniform18+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform19=countUniform19+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform20=countUniform20+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform21=countUniform21+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform22=countUniform22+1}}}
-  #ABOVE IS ALL POT 1 PERMUTATIONS WITH BOTH GROUP 1 AND 2 - BELOW IS POT2 AND 3 PERMUATIONS
-  if(validity[r]==1){for(k in 1:16){if("CONMEBOL4" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform23=countUniform23+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONMEBOL4" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform24=countUniform24+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONMEBOL4" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform25=countUniform25+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CONMEBOL4" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform26=countUniform26+1}}}
-  if(validity[r]==1){for(k in 1:16){if("EU11" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform27=countUniform27+1}}}
-  if(validity[r]==1){for(k in 1:16){if("EU11" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform28=countUniform28+1}}}
-  if(validity[r]==1){for(k in 1:16){if("EU11" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform29=countUniform29+1}}}
-  if(validity[r]==1){for(k in 1:16){if("EU11" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform30=countUniform30+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CAF1" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform31=countUniform31+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CAF1" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform32=countUniform32+1}}}
-  if(validity[r]==1){for(k in 1:16){if("CAF1" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform33=countUniform33+1}}}
-  if(validity[r]==1){for(k in 1:16){if("AFC1" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform34=countUniform34+1}}}
-  if(validity[r]==1){for(k in 1:16){if("AFC1" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform35=countUniform35+1}}}
-  if(validity[r]==1){for(k in 1:16){if("AFC1" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform36=countUniform36+1}}}
-  if(validity[r]==1){draws[[r]]=initialgroups}
-  
+  validity = prod(valid) 
+  # if(validity==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "CAF1" %in% initialgroups[1,,k]){countUniform1=countUniform1+1}}}
+  # if(validity==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "EU11" %in% initialgroups[1,,k]){countUniform2=countUniform2+1}}}
+  # if(validity==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "CONMEBOL4" %in% initialgroups[1,,k]){countUniform3=countUniform3+1}}}
+  # if(validity==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "AFC1" %in% initialgroups[1,,k]){countUniform4=countUniform4+1}}}
+  # if(validity==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "CAF1" %in% initialgroups[1,,k]){countUniform5=countUniform5+1}}}
+  # if(validity==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "AFC1" %in% initialgroups[1,,k]){countUniform6=countUniform6+1}}}
+  # if(validity==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "EU11" %in% initialgroups[1,,k]){countUniform7=countUniform7+1}}}
+  # if(validity==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "CONMEBOL4" %in% initialgroups[1,,k]){countUniform8=countUniform8+1}}}
+  # if(validity==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "CAF1" %in% initialgroups[1,,k]){countUniform9=countUniform9+1}}}
+  # if(validity==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "EU11" %in% initialgroups[1,,k]){countUniform10=countUniform10+1}}}
+  # if(validity==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "AFC1" %in% initialgroups[1,,k]){countUniform11=countUniform11+1}}}
+  # if(validity==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform12=countUniform12+1}}}
+  # if(validity==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform13=countUniform13+1}}}
+  # if(validity==1){for(k in 1:16){if("CONCACAF1" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform14=countUniform14+1}}}
+  # if(validity==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform15=countUniform15+1}}}
+  # if(validity==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform16=countUniform16+1}}}
+  # if(validity==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform17=countUniform17+1}}}
+  # if(validity==1){for(k in 1:16){if("EU1" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform18=countUniform18+1}}}
+  # if(validity==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform19=countUniform19+1}}}
+  # if(validity==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform20=countUniform20+1}}}
+  # if(validity==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform21=countUniform21+1}}}
+  # if(validity==1){for(k in 1:16){if("CONMEBOL1" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform22=countUniform22+1}}}
+  # #ABOVE IS ALL POT 1 PERMUTATIONS WITH BOTH GROUP 1 AND 2 - BELOW IS POT2 AND 3 PERMUATIONS
+  # if(validity==1){for(k in 1:16){if("CONMEBOL4" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform23=countUniform23+1}}}
+  # if(validity==1){for(k in 1:16){if("CONMEBOL4" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform24=countUniform24+1}}}
+  # if(validity==1){for(k in 1:16){if("CONMEBOL4" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform25=countUniform25+1}}}
+  # if(validity==1){for(k in 1:16){if("CONMEBOL4" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform26=countUniform26+1}}}
+  # if(validity==1){for(k in 1:16){if("EU11" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform27=countUniform27+1}}}
+  # if(validity==1){for(k in 1:16){if("EU11" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform28=countUniform28+1}}}
+  # if(validity==1){for(k in 1:16){if("EU11" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform29=countUniform29+1}}}
+  # if(validity==1){for(k in 1:16){if("EU11" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform30=countUniform30+1}}}
+  # if(validity==1){for(k in 1:16){if("CAF1" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform31=countUniform31+1}}}
+  # if(validity==1){for(k in 1:16){if("CAF1" %in% initialgroups[1,,k] && "AFC5" %in% initialgroups[1,,k]){countUniform32=countUniform32+1}}}
+  # if(validity==1){for(k in 1:16){if("CAF1" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform33=countUniform33+1}}}
+  # if(validity==1){for(k in 1:16){if("AFC1" %in% initialgroups[1,,k] && "CONCACAF4" %in% initialgroups[1,,k]){countUniform34=countUniform34+1}}}
+  # if(validity==1){for(k in 1:16){if("AFC1" %in% initialgroups[1,,k] && "CAF9" %in% initialgroups[1,,k]){countUniform35=countUniform35+1}}}
+  # if(validity==1){for(k in 1:16){if("AFC1" %in% initialgroups[1,,k] && "OFC1" %in% initialgroups[1,,k]){countUniform36=countUniform36+1}}}
+  if(validity==1){ totalvalid=totalvalid+1
+    draws[[totalvalid]]=initialgroups
+    print(paste("Simulation",totalvalid))}
 } 
 
 
-probofvalid = sum(validity)/length(validity)
-total = sum(validity)
+# probofvalid = sum(validity)/length(validity)
+total = length(draws)
 
 
-mean(probofvalid)
-var(probofvalid)
+# mean(probofvalid)
+# var(probofvalid)
 # 
 # p2.1 = countUniform1*100/total #5.74%
 # p2.2 = countUniform2*100/total #5.82%
@@ -204,9 +206,9 @@ countMatrix3U=countMatrix3U/total;countMatrix3U
 ### VARIANCES
 
 
-varmatrix1 = countMatrixU * (1 - countMatrixU) / sqrt(total)
+varmatrix1 = countMatrixU * (1 - countMatrixU) / total
 round(varmatrix1, 4)
-confintmatrix1 = qnorm(0.975, 0, 1) * varmatrix1
+confintmatrix1 = qnorm(0.975, 0, 1) * sqrt(varmatrix1)
 round(confintmatrix1, 4)
 lbmatrix1 = countMatrixU - confintmatrix1
 round(lbmatrix1, 4)
@@ -214,9 +216,9 @@ ubmatrix1 = countMatrixU + confintmatrix1
 round(ubmatrix1, 4)
 
 
-varmatrix2 = countMatrix2U * (1 - countMatrix2U) / sqrt(total)
+varmatrix2 = countMatrix2U * (1 - countMatrix2U) / total
 round(varmatrix2, 4)
-confintmatrix2 = qnorm(0.975, 0, 1) * varmatrix2
+confintmatrix2 = qnorm(0.975, 0, 1) * sqrt(varmatrix2)
 round(confintmatrix2, 4)
 lbmatrix2 = countMatrix2U - confintmatrix2
 round(lbmatrix2, 4)
@@ -224,13 +226,11 @@ ubmatrix2 = countMatrix2U + confintmatrix2
 round(ubmatrix2, 4)
 
 
-varmatrix3 = countMatrix3U * (1 - countMatrix3U) / sqrt(total)
+varmatrix3 = countMatrix3U * (1 - countMatrix3U) / total
 round(varmatrix3, 4)
-confintmatrix3 = qnorm(0.975, 0, 1) * varmatrix3
+confintmatrix3 = qnorm(0.975, 0, 1) * sqrt(varmatrix3)
 round(confintmatrix3, 4)
 lbmatrix3 = countMatrix3U - confintmatrix3
 round(lbmatrix3, 4)
 ubmatrix3 = countMatrix3U + confintmatrix3
 round(ubmatrix3, 4)
-
-
